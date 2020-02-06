d1ba73e4655012724371b61d4da8bf210f427b6b
Jonathan <johjo@ytreza.org>
2020-02-06 23:12:37 +0100

    return number when the number is prime

--- src/tests.rs
+++ src/tests.rs
@@ -1,10 +1,19 @@
 #[test]
-
 fn should_return_empty_list_with_number_below_or_equal_to_one() {
     assert_eq!(primefactor(0), []);
     assert_eq!(primefactor(1), []);
 }
 
+#[test]
+fn should_return_number_when_is_prime() {
+    assert_eq!(primefactor(2), [2]);
+    assert_eq!(primefactor(3), [3]);
+}
+
+
 fn primefactor(number: u32) -> Vec<u32> {
+    if number > 1 {
+        return [number].to_vec();
+    }
     [].to_vec()
 }
