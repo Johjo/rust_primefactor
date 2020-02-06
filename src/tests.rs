ca122b5987247496151bcde4ea2ab6199fcf02b5
Jonathan <johjo@ytreza.org>
2020-02-06 23:24:41 +0100

    return factors for multiple of two

--- src/tests.rs
+++ src/tests.rs
@@ -10,10 +10,25 @@ fn should_return_number_when_is_prime() {
     assert_eq!(primefactor(3), [3]);
 }
 
+#[test]
+fn should_return_multiple_numbers_when_is_not_prime() {
+    assert_eq!(primefactor(4), [2, 2]);
+    assert_eq!(primefactor(6), [2, 3]);
+    assert_eq!(primefactor(8), [2, 2, 2]);
+}
+
+
 
 fn primefactor(number: u32) -> Vec<u32> {
+    primefactor_rec(number, [].to_vec())
+}
+
+fn primefactor_rec(number: u32, factors: Vec<u32>) -> Vec<u32> {
     if number > 1 {
-        return [number].to_vec();
+        if number % 2 == 0 {
+            return primefactor_rec(number / 2, [factors, [2].to_vec()].concat());
+        }
+        return [factors, [number].to_vec()].concat();
     }
-    [].to_vec()
+    factors
 }
