32481d6c37f79ffce62c25514f3a494b1d137cee
Jonathan <johjo@ytreza.org>
2020-02-06 23:10:30 +0100

    return empty list when number are below or equal to one

--- src/tests.rs
+++ src/tests.rs
@@ -1,4 +1,10 @@
 #[test]
-fn test() {
-    assert_eq!("1", "1");
+
+fn should_return_empty_list_with_number_below_or_equal_to_one() {
+    assert_eq!(primefactor(0), []);
+    assert_eq!(primefactor(1), []);
+}
+
+fn primefactor(number: u32) -> Vec<u32> {
+    [].to_vec()
 }
